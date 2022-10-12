b0e79c7d7954dff2afce3ce26e819244c20517b5
nemo0 <subhachanda88@gmail.com>
2022-10-13 00:30:27 +0530

    Removed unnecessary packages

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config({ path: './config/.env' });
+dotenv.config({ path: './.env' });
 
 const connectDB = async () => {
   try {
